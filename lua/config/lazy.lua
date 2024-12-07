1caeeba41810792b4333ad3e3ebc0b93f5fcd557
luko <lucas.prm04@gmail.com>
2024-12-07 13:57:43 +0100

    fufa

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,6 +28,21 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
 	'machakann/vim-highlightedyank',
 	require 'plugins.test',
+	
+	{
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function () 
+	local configs = require("nvim-treesitter.configs")
+
+	configs.setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },  
+			})
+	end
+	},
 
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
